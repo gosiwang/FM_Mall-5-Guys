2f9f27fa5f0b468809474ab3ae0588d88e16283a
jaeseung9 <seojaeseung9@gmail.com>
2025-12-08 18:30:57 +0900

    컨트롤러 공용 함수 추가

---
+++ fmmall/src/main/java/com/sesac/fmmall/Controller/BaseController.java
@@ -0,0 +1,12 @@
+package com.sesac.fmmall.Controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public abstract class BaseController {
+
+    protected int getCurrentUserId() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return (int) auth.getPrincipal();
+    }
+}
