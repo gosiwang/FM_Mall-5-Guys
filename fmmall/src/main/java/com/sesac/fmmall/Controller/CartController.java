eadffb80df1ad2b1026a9d7f95021aadb5c348cc
moonwonkim-a <happynei200@gmail.com>
2025-12-08 18:44:57 +0900

    feat : BaseController 추가 및 controller 로직 ì수ì 

--- fmmall/src/main/java/com/sesac/fmmall/Controller/CartController.java
+++ fmmall/src/main/java/com/sesac/fmmall/Controller/CartController.java
@@ -4,85 +4,69 @@ import com.sesac.fmmall.DTO.CartItem.CartItemCheckStatusRequestDTO;
 import com.sesac.fmmall.DTO.CartItem.CartItemCreateRequestDTO;
 import com.sesac.fmmall.DTO.CartItem.CartItemUpdateRequestDTO;
 import com.sesac.fmmall.DTO.CartResponseDTO;
-import com.sesac.fmmall.DTO.User.UserResponseDto;
 import com.sesac.fmmall.Service.CartService;
-import com.sesac.fmmall.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Cart")
 @RequiredArgsConstructor
-public class CartController {
+public class CartController extends BaseController {
 
     private final CartService cartService;
-    private final UserService userService;
 
     // 장바구니 상품 추가
     @PostMapping("/insert/")
     public ResponseEntity<CartResponseDTO> addCartItem(
-            @RequestBody CartItemCreateRequestDTO requestDTO,
-            @AuthenticationPrincipal int userId
+            @RequestBody CartItemCreateRequestDTO requestDTO
     ) {
-        UserResponseDto dto = userService.getUserInfo(userId);
-
-        CartResponseDTO response = cartService.createCartItem(dto.getId(), requestDTO);
+        CartResponseDTO response = cartService.createCartItem(getCurrentUserId(), requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     // 장바구니 상품 수량 변경
     @PutMapping("/modify/{cartItemId}")
     public ResponseEntity<CartResponseDTO> modifyCartItem(
-            @AuthenticationPrincipal int userId,
             @PathVariable int cartItemId,
             @RequestBody CartItemUpdateRequestDTO requestDTO
     ) {
-        UserResponseDto dto = userService.getUserInfo(userId);
-
-        CartResponseDTO response = cartService.updateCartItemQuantity(dto.getId(), cartItemId, requestDTO);
+        CartResponseDTO response = cartService.updateCartItemQuantity(getCurrentUserId(), cartItemId, requestDTO);
         return ResponseEntity.ok(response);
     }
 
-    // [추가될 메서드] 장바구니 상품 체크 상태 변경
+    // 장바구니 상품 체크상태 변경
     @PutMapping("/check/{cartItemId}")
     public ResponseEntity<CartResponseDTO> checkCartItem(
-            @AuthenticationPrincipal int userId,
             @PathVariable int cartItemId,
             @RequestBody CartItemCheckStatusRequestDTO requestDTO
     ) {
-        CartResponseDTO response = cartService.updateCartItemCheckStatus(userId, cartItemId, requestDTO);
+        CartResponseDTO response = cartService.updateCartItemCheckStatus(getCurrentUserId(), cartItemId, requestDTO);
         return ResponseEntity.ok(response);
     }
 
     // 장바구니 상품 삭제
     @DeleteMapping("/delete/{cartItemId}")
     public ResponseEntity<Void> deleteCartItem(
-            @AuthenticationPrincipal int userId,
             @PathVariable int cartItemId
     ) {
-        UserResponseDto dto = userService.getUserInfo(userId);
-        cartService.removeCartItem(dto.getId(), cartItemId);
+        cartService.removeCartItem(getCurrentUserId(), cartItemId);
         return ResponseEntity.noContent().build();
     }
 
     // 장바구니 전체 삭제 (유저 기준)
     @DeleteMapping("/deleteAll")
-    public ResponseEntity<Void> clearCart(@AuthenticationPrincipal int userId) {
-        UserResponseDto dto = userService.getUserInfo(userId);
-
-        cartService.clearCart(dto.getId());
+    public ResponseEntity<Void> clearCart() {
+        cartService.clearCart(getCurrentUserId());
         return ResponseEntity.noContent().build();
     }
 
     // 장바구니 목록 조회 (유저 기준)
     @GetMapping("/findAll")
-    public ResponseEntity<CartResponseDTO> findAllCartItems(@AuthenticationPrincipal int userId) {
-        UserResponseDto dto = userService.getUserInfo(userId);
+    public ResponseEntity<CartResponseDTO> findAllCartItems() {
 
-        CartResponseDTO response = cartService.findAllCartItems(dto.getId());
+        CartResponseDTO response = cartService.findAllCartItems(getCurrentUserId());
         return ResponseEntity.ok(response);
     }
 }
